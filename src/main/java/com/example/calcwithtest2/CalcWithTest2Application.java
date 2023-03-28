40fa25e9fbdc06da54c7c332d4d4c2880a38fefb
Maks <max@moais.ru>
2023-03-28 10:17:04 +0300

    initial commit new project

---
+++ src/main/java/com/example/calcwithtest2/CalcWithTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.calcwithtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcWithTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcWithTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calcwithtest2/CalcWithTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calcwithtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcWithTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
